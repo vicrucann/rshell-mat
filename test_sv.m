6eea94bda008870300988b7c2c49b1d25421697b
Victoria Rudakova <vicrucann@gmail.com>
2015-05-14 15:35:13 -0400

    test file concept

---
+++ test_sv.m
@@ -0,0 +1,9 @@
+% test_sv
+% Generate two matrices - m1 and m2
+% Divide them into n parts (n servers available)
+% Save parts as files
+% Launch sv script, pass files names as input parameters, as well as ip
+% addresses of the servers
+% When results are ready, load them back to matlab workspace
+% Assert the results are correct
+
