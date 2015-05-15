8293d5d9cb86001ce634734645c1a51c005fe16a
Victoria Rudakova <vicrucann@gmail.com>
2015-05-15 16:04:54 -0400

    reading the input arguments done

--- dserver.sh
+++ dserver.sh
@@ -6,3 +6,39 @@
 #   Collect and merge the results.
 
 
+args=("$@")
+printf "Number of arguments passed: %d\n" $#
+nargs=$#
+if [ $nargs -lt 6 ]; then
+  echo "ERROR: Number of passed arguments is smaller than required minimum (6)"
+  exit 1
+fi
+nservs=$((nargs-5))
+printf "Number of servers: %d\n" $nservs
+
+LOGIN=${args[0]}
+PPATH=${args[1]}
+i=2
+while true; do
+  IPADDRS[$((i-2))]=${args[$i]}
+  i=$((i+1))
+  nservs=$((nservs-1))
+  if (( nservs <= 0 )); then
+    printf "IP addresses extracted:\n"
+    echo ${IPADDRS[@]} 
+    break
+  fi
+done
+
+REMMAT=${args[$i]}
+VARMAT=${args[$(($i+1))]}
+SLEEPTIME=${args[$(($i+2))]}
+printf "Finished reading the input arguments\n"
+
+i=0
+for IPA in ${IPADDRS[@]}; do
+  FDONE[$i]=0
+  i=$((i+1))
+done
+
+
