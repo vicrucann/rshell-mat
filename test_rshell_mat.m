83f0f3724cd6f2e605bdbe13eda771460f6ce478
Victoria Rudakova <vicrucann@gmail.com>
2015-05-15 14:36:57 -0400

    mandelbrot part done

--- test_rshell_mat.m
+++ test_rshell_mat.m
@@ -2,10 +2,52 @@
 % Two examples are considered: calculation of madelbrodt set and two
 % matricies summation
 
+%% Setting up
+login = 'cryo';
+ppath = '/home/cryo/tester';
+ipaddrs = ['172.23.2.105' ' ' '172.23.5.77'];
+remmat = ''; % definition given below for each example case
+varmat = ''; % definition given below for each example case
+sleeptime = 10;
+bashscript = fullfile(pwd,'cluster.sh'); % main bash script that organizes data processing
+
+[ncluster ~] = find(ipaddrs==' '); % to break data into n clusters (as many as given servers)
+ncluster = ncluster+1;
+
 %% Two matrices summation
 % Given two large matrices, find its sum
-a=1;
+remmat = 'sumvar.m';
 
-%% Mandelbrodt set
+%% Mandelbrot set
 % Given resolution and iteration number, find corresponding Mandelbrodt set
-a=2;
+
+% name of matlab function to be run on remote server
+remmat = 'mandelbrodt.m';
+varmat = 'mandelbrodt';
+
+% input parameters
+iter = 500;
+isize = 1000;
+xlim = [-2, 1]; % to split
+ylim = [-1.5, 1.5]; % to split
+x = linspace( xlim(1), xlim(2), isize );
+y = linspace( ylim(1), ylim(2), isize );
+[xGrid,yGrid] = meshgrid( x, y );
+szx = ceil(size(xGrid,2)/ncluster);
+
+% perform the split (assume we break along "X" dimension)
+for i=1:ncluster
+    if (i ~= ncluster)
+        xi=xGrid(:, szx*(i-1)+1:szx*i);
+        yi=yGrid(:, szx*(i-1)+1:szx*i);
+    else
+        xi=xGrid(:, szx*(i-1)+1:end);
+        yi=yGrid(:, szx*(i-1)+1:end);
+    end
+    save([varmat int2str(i) '.mat'], 'xi', 'yi', 'iter');
+end
+% assert the mentioned files exist(in bash file)
+cmdStr = [bashscript ' ' login ' ' ppath ' ' ipaddrs ' ' remmat ' ' varmat ' ' int2str(sleeptime)];
+% perform the command
+system(cmdStr);
+
