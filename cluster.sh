843786fd4289f8e55a35e34819a1ba8256321bcf
Victoria Rudakova <vicrucann@gmail.com>
2015-05-15 14:48:28 -0400

    renamed

--- cluster.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#
-# Perform heavy data processing on different servers by transferring the data and "kernel" function to the servers and then collection and merging the results.
-
-
---
+++ dserver.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Perform heavy data processing by splitting the task to different servers:
+#   Transfer the data and "kernel" function to the servers. 
+#   Wait for servers to complete.
+#   Collect and merge the results.
+
+
