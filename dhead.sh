de7b4aa96896b62e9f01b364058f8f85323a25e4
Victoria Rudakova <vicrucann@gmail.com>
2015-05-29 12:26:19 -0400

    pathout and pathmat added so that user can chose the source and work folders

--- dhead.sh
+++ dhead.sh
@@ -10,13 +10,15 @@
 # ================
 
 #printf "The n input arguments for dhead.sh script are: \n"
-#printf "[1] LOGIN : login id to the remote servers (assumed it's the same login for every server)\n"
-#printf "[2]- PPATH : working directory (will be created if does not exist) on the remote servers; assumed to be the same for each server \n"
-#printf "[3..n-4]- IPADDRS : range of ip-addresses of all the servers, assumed they have the same login/psw account \n"
-#printf "[n-3]- REMMAT : name of the matlab function (e.g. 'myfunc') that will be copied and launched on remote servers by dremote.sh \n"
-#printf "[n-2]- VARMAT : name of the workspace varialbes file (without numering and .mat); these are the variables to copy and load to matlab memory on the remote servers \n"
-#printf "[n-1]- SLEEPTIME : integer that indicates number of seconds to pause when waiting for each remote server to complete their computations \n"
-#printf "[n] - FRES : name of the local folder where the results will be copied to from the servers \n"
+#printf "[0] LOGIN : login id to the remote servers (assumed it's the same login for every server)\n"
+#printf "[1]- PPATH : working directory (will be created if does not exist) on the remote servers; assumed to be the same for each server \n"
+#printf "[2..n-7]- IPADDRS : range of ip-addresses of all the servers, assumed they have the same login/psw account \n"
+#printf "[n-6]- PATHMAT : path name where REMMAT function is located \n"
+#printf "[n-5]- REMMAT : name of the matlab function (e.g. 'myfunc') that will be copied and launched on remote servers by dremote.sh \n"
+#printf "[n-4]- PATHOUT : path name where VARMAT data will be saved to and loaded from \n"
+#printf "[n-3]- VARMAT : name of the workspace varialbes file (without numering and .mat); these are the variables to copy and load to matlab memory on the remote servers \n"
+#printf "[n-2]- SLEEPTIME : integer that indicates number of seconds to pause when waiting for each remote server to complete their computations \n"
+#printf "[n-1] - FRES : name of the local folder where the results will be copied to from the servers \n"
 
 
 # ARGUMENTS PARSING
@@ -25,11 +27,11 @@
 args=("$@")
 printf "\nNumber of arguments passed: %d\n" $#
 nargs=$#
-if [ $nargs -lt 7 ]; then
-	echo "ERROR: Number of passed arguments is smaller than required minimum (7)"
+if [ $nargs -lt 9 ]; then
+	echo "ERROR: Number of passed arguments is smaller than required minimum (8)"
 	exit 1
 fi
-nservs=$((nargs-6))
+nservs=$((nargs-8))
 printf "Number of servers: %d\n" $nservs
 
 LOGIN=${args[0]}
@@ -48,29 +50,33 @@ done
 printf "\nIP addresses extracted:\n"
 echo ${IPADDRS[@]}
 
-REMMAT=${args[$nargs-4]} # check file existance
-test -e $REMMAT.m
+PATHMAT=${args[$nargs-6]}
+
+REMMAT=${args[$nargs-5]} # check file existance
+test -e $PATHMAT$REMMAT.m
 if [ $? -ne 0 ]; then
-	printf "ERROR: no such file: %s\n" $REMMAT.m
+	printf "ERROR: no such file: %s\n" $PATHMAT$REMMAT.m
 	exit 1
 fi
-printf "Matlab script file for remote: %s\n" $REMMAT.m
+printf "Matlab script file for remote: %s\n" $PATHMAT$REMMAT.m
 
 REMSCRIPT="dserver.sh" # check file existance
-test -e $REMSCRIPT
+test -e $PATHMAT$REMSCRIPT
 if [ $? -ne 0 ]; then
-	printf "ERROR: no such file: %s\n" $REMSCRIPT
+	printf "ERROR: no such file: %s\n" $PATHMAT$REMSCRIPT
 	exit 1
 fi
-printf "Remote bash script: %s\n" $REMSCRIPT
+printf "Remote bash script: %s\n" $PATHMAT$REMSCRIPT
+
+PATHOUT=${args[$(($nargs-4))]}
 
 VARMAT=${args[$(($nargs-3))]} # check file existance
 j=1
 i=0
 for IPA in ${IPADDRS[@]}; do
-	test -e $VARMAT$j.mat
+	test -e $PATHOUT$VARMAT$j.mat
 	if [ $? -ne 0 ]; then
-		printf "ERROR: no such file: %s\n" $VARMAT$j.mat
+		printf "ERROR: no such file: %s\n" $PATHOUT$VARMAT$j.mat
 		exit 1
 	fi
 	FDONE[$i]=0
@@ -79,7 +85,7 @@ for IPA in ${IPADDRS[@]}; do
 	i=$((i+1))
 done
 printf "\nVariables-to-load extracted:\n"
-echo ${IFILES[@]}
+echo $PATHOUT${IFILES[@]}
 
 SLEEPTIME=${args[$(($nargs-2))]}
 printf "Pause time is set to %i\n" $SLEEPTIME
@@ -99,9 +105,9 @@ printf "\nFile transfer and script launching\n"
 for IPA in ${IPADDRS[@]}; do
 	ssh $LOGIN@$IPA "mkdir -p $PPATH" # create working directory, if necessary
 	ssh $LOGIN@$IPA "rm -f $PPATH/*" # clear the working directory from any previous data
-	scp $REMSCRIPT $LOGIN@$IPA:$PPATH 
-	scp $REMMAT.m $LOGIN@$IPA:$PPATH 
-	scp ${IFILES[$i]} $LOGIN@$IPA:$PPATH 
+	scp $PATHMAT$REMSCRIPT $LOGIN@$IPA:$PPATH 
+	scp $PATHMAT$REMMAT.m $LOGIN@$IPA:$PPATH 
+	scp $PATHOUT${IFILES[$i]} $LOGIN@$IPA:$PPATH 
 	
 	ssh -n -f $LOGIN@$IPA "sh -c 'cd $PPATH; chmod u+x $REMSCRIPT; nohup ./$REMSCRIPT $REMMAT ${IFILES[$i]} > $VARMAT.out 2> $VARMAT.err < /dev/null &'"
 	i=$((i+1))
