4703631b16e3f4ccc2292f068e06e4befff7c1bc
Victoria Rudakova <vicrucann@gmail.com>
2015-06-16 10:16:24 -0400

    cache var added on server script + vim folding

--- dserver.sh
+++ dserver.sh
@@ -6,18 +6,19 @@
 #	Generates output data
 #	2015 Victoria Rudakova, vicrucann@gmail.com
 
+#{{{# ARGUMENT PARSING
 # ================
-# ARGUMENT PARSING
 
 args=("$@")
 nargs=$#
-if [ $nargs -ne 2 ]; then
-	printf "ERROR: Number of passed parameters must be equal 2\n"
+if [ $nargs -ne 3 ]; then
+	printf "ERROR: Number of passed parameters must be equal 3\n"
 	exit 1
 fi
 
 REMMAT=${args[0]}
 IFILE=${args[1]}
+CFILE=${args[2]}
 RESFILE="result_$IFILE"
 
 test -e $REMMAT.m # check file exists
@@ -32,20 +33,30 @@ if [ $? -ne 0 ]; then
 	exit 1
 fi
 
+if [ $CFILE -ne 0 ]; then
+  # check file exists
+  test -e $CFILE
+  if [ $? -ne 0 ]; then
+    printf "Error on remote: no such file - %s\n" $CFILE
+    exit 1
+  fi
+fi
+
 printf "\nRemote matlab script name: %s\n" $REMMAT
 printf "Input file name: %s\n" $IFILE
-printf "Result save name: %s\n\n" $RESFILE
+printf "Cache file (0 if nonoe): %s\n" $CFILE
+printf "Result save name: %s\n\n" $RESFILE#}}}
 
+#{{{# LAUNCH THE MATLAB FUNCTION
 # ================
-# LAUNCH THE MATLAB FUNCTION
 
 #printf "\nRunning matlab script\n"
-matlab -nodisplay -nojvm -nosplash -nodesktop -wait -r "$REMMAT('$IFILE','$RESFILE');quit();"
-#printf "\nMatlab work done\n"
+matlab -nodisplay -nojvm -nosplash -nodesktop -wait -r "$REMMAT('$IFILE','$RESFILE','$CFILE');quit();"
+#printf "\nMatlab work done\n"#}}}
 
+#{{{# INDICATE MATLAB HAD FINISHED CALCULATIONS
 # ================
-# INDICATE MATLAB HAD FINISHED CALCULATIONS
 
 > dserver.dn
-#printf "\nresult.dn file generated\n"
+#printf "\nresult.dn file generated\n"#}}}
 
