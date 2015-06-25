e7148dedc55f51ce2db8cff6177a55c948e565c3
Victoria Rudakova <vicrucann@gmail.com>
2015-06-25 14:12:09 -0400

    printf-s added to track the steps

--- dtest.sh
+++ dtest.sh
@@ -3,16 +3,20 @@
 # A script to test accessibility to spicified IP addresses
 # Also tests if the private key is added (ssh-add)
 
+printf "Testing if SSH connection can be set up successfully\n"
 args=("$@")
 nargs=$#
+printf "Number of input args is %i\n" $nargs
 if [ $nargs -lt 2  ]; then
   printf "ERROR: There must be at least two passed parameters (login and IP address)\n"
   exit 1
 fi
 
 LOGIN=${args[0]}
+printf "Login is %s\n" $LOGIN
 
 nservs=$((nargs-1))
+printf "Number of servers is %i\n" $nservs
 i=1
 while true; do
   IPADDRS[$((i-1))]=${args[$i]}
@@ -22,14 +26,17 @@ while true; do
     break
   fi
 done
+printf "Done reading the IP addresses\n"
 
 for IPA in ${IPADDRS[@]}; do
+  printf "Trying to SSH to server %s\n" $IPA
   ssh -q $LOGIN@$IPA exit
   status=$?
   if [ $status -eq 255 ]; then
     printf "ERROR: server %s is not accessible, check connectivity and private key setup\n"
     exit 1
   fi
+  printf " - success\n"
 done
-
+printf "Done checking connectivity\n"
 
